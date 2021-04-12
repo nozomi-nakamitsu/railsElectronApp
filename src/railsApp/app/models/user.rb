0dcda23ee9fae66023e06e6bebdc2c426d52d7de
nozomi-nakamitsu <nozomi_nakamitsu@arsaga.jp>
2021-04-12 12:03:14 +0900

    ログイン時間を制限

--- src/railsApp/app/models/user.rb
+++ src/railsApp/app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable,:timeoutable,
          :recoverable, :rememberable, :validatable
   #  半角英数字だけ許可する
   VALID_UID_REGEX = /\A[a-z0-9]+\z/i
