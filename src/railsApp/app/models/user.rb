e6a8af4b84353a7b4f9aae4b5c9ac5855799c9e4
nozomi-nakamitsu <nozomi_nakamitsu@arsaga.jp>
2021-04-12 11:09:35 +0900

    UIDにバリテーション追加

--- src/railsApp/app/models/user.rb
+++ src/railsApp/app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  #  半角英数字だけ許可する
+  VALID_UID_REGEX = /\A[a-z0-9]+\z/i
+  validates :uid, format: { with: VALID_UID_REGEX }
 end
